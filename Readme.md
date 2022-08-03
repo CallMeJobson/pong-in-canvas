a7de1ef75a77e6923efb2231d0fc02203c0d3d14
Johnsonj91 <Jordan33111@gmail.com>
2022-08-02 20:21:29 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Pong in canvas
+
+Working a pong game in canvas
+
+
+## How It's Made:
+Tech used: HTML, CSS, JavaScript
+
+
+## Optimizations 
+
+
+## Lessoned Learned
+
+
+
+
---
+++ app.js
@@ -0,0 +1,40 @@
+var canvas;
+var canvasContext
+var ballX = 50
+window.onload = function(){
+    canvas = document.getElementById("gameCanvas");
+    canvasContext = canvas.getContext("2d");
+
+
+    var framesPerSecond = 30
+    setInterval(function(){
+        moveEverything();
+        drawEverything();
+    }, 1000/ framesPerSecond)
+    
+    
+}
+
+
+function moveEverything(){
+    ballX += 5;
+}
+
+function drawEverything(){
+    
+    canvasContext.fillStyle = 'black';
+    //0 0 is the position of what we what to draw
+    canvasContext.fillRect(0,0,canvas.width, canvas.height);
+
+    canvasContext.fillStyle = 'white';
+    //0 0 is the position of what we what to draw
+    canvasContext.fillRect(0,200,10, 100);
+
+    canvasContext.fillStyle = 'red';
+    //0 0 is the position of what we what to draw
+    canvasContext.fillRect(ballX,100,10, 10);
+
+    
+
+    //Draw order is important 
+}
